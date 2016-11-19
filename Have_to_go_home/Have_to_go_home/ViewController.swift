36b94c9b460b59f9e38c98d2da34c840341442a3
Mac <mac@lendit.co.kr>
2016-11-19 14:28:44 +0900

    프로젝트 초기 구조 잡기1

---
+++ Have_to_go_home/Have_to_go_home/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Have_to_go_home
+//
+//  Created by 유정우 on 2016. 11. 19..
+//  Copyright © 2016년 유정우. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
